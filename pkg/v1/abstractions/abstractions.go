f9ae1fc396c2cd31305a3672069811a42b0a4c33
Jon Johnson <jonjohnson@google.com>
2019-02-21 13:06:24 -0800

    abstractions

--- pkg/v1/abstractions/abstractions.go
+++ pkg/v1/abstractions/abstractions.go
@@ -1,6 +1,7 @@
 package abstractions
 
 import (
+	"github.com/google/go-containerregistry/pkg/name"
 	v1 "github.com/google/go-containerregistry/pkg/v1"
 	"github.com/google/go-containerregistry/pkg/v1/types"
 )
@@ -9,14 +10,32 @@ import (
 
 // Source represents an abstraction from which we can read artifacts.
 //
+// Descriptors() and Tags() are useful for discovering what's available from a Source.
+// Image(), Index(), and Layer() are useful for actually fetching content from that Source.
+//
 // For example:
-// * a tarball can contain multiple images.
-// * a remote repository can act as a source, but there's no standard way to implement Descriptors()
+// * a tarball can contain multiple images, exposing its contents via Tags.
+// * a remote repository can act as a source, exposing its contents via Tags (/tags/list).
+// * any image index can act as a source, exposing its children via Descriptors.
+// * an OCI image layout acts as a source, exposing contents of index.json via Descriptors.
 type Source interface {
 	Descriptors() []v1.Descriptor
+	Tags() []name.Tag
+
 	Image(v1.Hash) v1.Image
 	Index(v1.Hash) v1.ImageIndex
 	Layer(v1.Hash) v1.Layer
+
+	// This could be a name.Tag or a v1.Hash or a name.Digest but golang doesn't have method overloading :(
+	// Not sure best way to handle it.
+	//
+	// Perhaps "ImagePointer" or something would be the union of (name.Tag + name.Digest + v1.Hash).
+	//
+	// name.Reference is almost correct, but sometimes we have a v1.Hash without a repo.
+	// We could use them for sentinel repos?
+	//
+	// The Source probably knows if the string is supposed to be a v1.Hash or a name.Reference...
+	ImageDescriptor(string) ImageDescriptor
 }
 
 // Sink represents an abstraction to which we can write artifacts.
@@ -26,13 +45,13 @@ type Sink interface {
 	Layer(v1.Layer) error
 }
 
-// Helpful concepts?
+// Useful ideas?
 
-// ImageHandle represents a union of v1.Image and v1.ImageIndex.
+// ImageDescriptor represents a union of v1.Image and v1.ImageIndex.
 //
 // This is useful in cases where we read an image from a Source
 // and don't know ahead of time what format it's in.
-type ImageHandle interface {
+type ImageDescriptor interface {
 	Descriptor() v1.Descriptor
 	Image(v1.Hash) v1.Image
 	Index(v1.Hash) v1.ImageIndex
